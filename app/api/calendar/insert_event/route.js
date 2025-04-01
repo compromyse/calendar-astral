9b01e987d2f4378316f7c697a102035e53924998
Raghuram Subramani <raghus2247@gmail.com>
2025-04-01 22:49:21 +0530

    api: create endpoint to insert one-off events

---
+++ app/api/calendar/insert_event/route.js
@@ -0,0 +1,30 @@
+"use server";
+
+import { createClient } from '@/utils/supabase/server';
+import { authenticateRequest } from '@/utils/auth';
+import { insertNewEvent } from '@/utils/calendar/insert';
+import { fetchEvents } from '@/utils/calendar/fetch';
+
+export async function POST(request) {
+  const supabase = await createClient();
+
+  let { user, error, status } = await authenticateRequest(request, supabase);
+  
+  if (error) {
+    return Response.json({ error }, { status });
+  }
+
+  const body = await request.json();
+
+  let { data, err } = await insertNewEvent(supabase, {
+    user_id: user.id,
+    title: body.title,
+    date: body.date
+  });
+
+  if (err) {
+    return Response.json({ error: err }, { status: 500 });
+  }
+
+  return Response.json({ data });
+}
