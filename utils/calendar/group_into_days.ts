55a5871312cdeea890af5571b67e355a4fb20928
Raghuram Subramani <raghus2247@gmail.com>
2025-04-05 14:54:58 +0530

    ui: pre-calculate event movement conditions

--- utils/calendar/group_into_days.ts
+++ utils/calendar/group_into_days.ts
@@ -32,7 +32,7 @@ export function groupIntoDays(events: Event[], weekStart: Date): CalendarDay[] {
     grouped[eventDateKey].events.push({
       id: event.id,
       title: event.title,
-      date: event.date
+      canBeTouched: new Date(event.date) >= new Date()
     });
   });
 
